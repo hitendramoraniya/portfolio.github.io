8bef18b164234dd23c267c2ebe072a1a9243de23
hitendra <hitendramoraniya@gmail.com>
2025-01-22 18:19:27 +0530

    update code

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "009c9e65172e010890f7f65fde438006",
-"index.html": "ece3c7327e03dbcb942840ef8c04e8f7",
-"/": "ece3c7327e03dbcb942840ef8c04e8f7",
-"main.dart.js": "f7630801dadc02392fc3404b379c55b4",
+"index.html": "736a211e4b4722d7d9ac927f12162883",
+"/": "736a211e4b4722d7d9ac927f12162883",
+"main.dart.js": "882ccaf507a4b8458b0fc3799025c9f6",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
